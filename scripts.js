a254cdd3da77e97a215de2fae2dfbc2477e87de6
vinisilva07 <165903186+vinisilva07@users.noreply.github.com>
2025-04-08 14:24:31 +0000

    arrumando menu acessibilidade

--- scripts.js
+++ scripts.js
@@ -1,14 +1,33 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const botaoAumentarFonte = document.getElementById('aumentar-fonte')
+    const botaoAumentar = document.getElementById("aumentar-fonte")
     //Pega o botão de aumentar fonte e coloca na constante
 
     let tamanhoDaFonte = 1
     // define o tamanho da fonte na variável
 
-    botaoAumentarFonte.addEventListener('click', function(){
+    botaoAumentar.addEventListener('click', function(){
         tamanhoDaFonte = tamanhoDaFonte + 0.1
         document.body.style.fontSize = `${tamanhoDaFonte}rem`
+
     })
+  
+    const botaoDiminuir = document.getElementById("diminuir-fonte")
+    
+        botaoDiminuir.addEventListener('click', function(){
+            tamanhoDaFonte = tamanhoDaFonte - 0.1
+            document.body.style.fontSize = `${tamanhoDaFonte}rem`
+    })
+
+    const botaoAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesAcessibilidade = document.getElementById('opcoes-acessibilidade')
+    
+    botaoAcessibilidade.addEventListener('click', function (){
+        botaoAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesAcessibilidade.classList.toggle('apresenta-lista')
+       })
+       
+
 
 
 })
+
