6e83c0b1297309f4ca06500eac424ee45b8669e8
vinisilva07 <165903186+vinisilva07@users.noreply.github.com>
2025-04-15 14:36:18 +0000

    contraste no site

--- scripts.js
+++ scripts.js
@@ -27,7 +27,10 @@ document.addEventListener('DOMContentLoaded', function(){
        })
        
 
+       const alternaContraste = document.getElementById('alterna-contraste')
+       alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
 
-
+        
 })
-
+})
