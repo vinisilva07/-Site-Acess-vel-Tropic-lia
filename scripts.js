86df424ffbfd3bdd7ebbb19df81f7093ebbc15e7
vinisilva07 <165903186+vinisilva07@users.noreply.github.com>
2025-04-22 14:46:06 +0000

    arrumando o desajeito

--- scripts.js
+++ scripts.js
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function(){
        const alternaContraste = document.getElementById('alterna-contraste')
        alternaContraste.addEventListener('click', function(){
         document.body.classList.toggle('alto-contraste')
-
-        
+        const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+         
 })
 })
